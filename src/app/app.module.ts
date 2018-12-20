c476691c981ff7cd9365b3c8ed7f0a506e5cfecf
songzoon <46012083+songzoon@users.noreply.github.com>
2018-12-20 09:00:06 +0700

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BarcodeScannerPageModule } from './../pages/barcode-scanner/barcode-scanner.module';
+import { BarcodeScannerPage } from './../pages/barcode-scanner/barcode-scanner';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { SocialInfoPage } from '../pages/social-info/social-info';
+import { SMS } from '@ionic-native/sms'; // import plugin SMS
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';  // import plugin BarcodeScanner
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,SocialInfoPage,BarcodeScannerPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,SocialInfoPage,BarcodeScannerPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    SMS, // เพิ่ม เพื่อให้เรียกใชฟังชั่น SMS
+    BarcodeScanner,  // เพิ่ม เพื่อให้เรียกใชฟังชั่น BarcodeScanner
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
